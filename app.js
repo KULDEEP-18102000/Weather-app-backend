48c2655ceb7025311aee4b51a07a083d30ecae1e
KULDEEP-18102000 <jadonkuldeepsingh2@gmail.com>
2024-09-11 21:37:22 +0530

    removed comments

--- app.js
+++ app.js
@@ -23,21 +23,6 @@ app.use(BodyParser.json({extended:false}))
 app.use('/weather',weatherRoutes)
 
 
-
-// mongoose
-  // .connect('mongodb+srv://jadonkuldeepsingh2:cK85oQfwJfbt1yML@cluster0.izbkthq.mongodb.net/', {
-    // useNewUrlParser: true, // The underlying MongoDB driver has deprecated their current connection string parser. Because this is a major change, they added the useNewUrlParser flag to allow users to fall back to the old parser if they find a bug in the new parser.
-    // useCreateIndex: true, // Again previously MongoDB used an ensureIndex function call to ensure that Indexes exist and, if they didn't, to create one. This too was deprecated in favour of createIndex . the useCreateIndex option ensures that you are using the new function calls.
-    // useFindAndModify: false, // findAndModify is deprecated. Use findOneAndUpdate, findOneAndReplace or findOneAndDelete instead.
-    // useUnifiedTopology: true, // Set to true to opt in to using the MongoDB driver's new connection management engine. You should set this option to true , except for the unlikely case that it prevents you from maintaining a stable connection.
-  // })
-  // .then((con) => {
-    // console.log("DB Connection successful");
-    // app.listen(3000)
-    // console.log("server listening on port 3000")
-  // });
-
-
   mongoose
   .connect('mongodb://localhost:27017/Weather', {
     useNewUrlParser: true,  // Use the new URL parser for MongoDB
